7ee1b93658f1c846f7235867b23a4b0b37e50003
Unknown <vez64ryj@gmail.com>
2019-02-22 22:52:21 +0100

    Started voxels

---
+++ Assets/Scripts/VoxelHair.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelHair : MonoBehaviour
+{
+    private Vector4[,,] voxels;
+
+    [SerializeField]
+    private Vector3Int size;
+
+    private GameObject[] voxelObjects;
+    [SerializeField]
+    private GameObject testObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        voxels = new Vector4[size.x,size.y,size.z];
+        voxelObjects = new GameObject[size.x * size.y * size.z];
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j< size.y; j++)
+            {
+                for (int k = 0; k < size.z; k++)
+                {
+                    voxelObjects[i] = Instantiate(testObject, new Vector3(i,j,k),Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
