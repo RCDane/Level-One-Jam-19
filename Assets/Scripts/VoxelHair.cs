54d668a5114c79fa47a66c4a706b6589701fcb2f
Unknown <vez64ryj@gmail.com>
2019-02-23 21:41:00 +0100

    hairtool prefab

--- Assets/Scripts/VoxelHair.cs
+++ Assets/Scripts/VoxelHair.cs
@@ -36,10 +36,10 @@ public class VoxelHair : MonoBehaviour
             {
                 for (int k = 0; k < size.z; k++)
                 {
-                    if(i==0 || j == 0 || k == 0 || i+1 == size.x || j + 1 == size.y|| k + 1 == size.y)
-                        voxels[i, j, k] = new Voxel(new Vector4(i, j, k, 1f), i + size.y * j + size.y * size.z * k, new Vector3Int(i, j, k),true);
+                    if (i == 0 || j == 0 || k == 0 || i + 1 == size.x || j + 1 == size.y || k + 1 == size.y)
+                        voxels[i, j, k] = new Voxel(new Vector4(i, j, k, 1f), i + size.y * j + size.y * size.z * k, new Vector3Int(i, j, k), true);
                     else
-                        voxels[i, j, k] = new Voxel(new Vector4(i, j, k, 1f), i + size.y * j + size.y*size.z * k, new Vector3Int(i, j, k),true);
+                        voxels[i, j, k] = new Voxel(new Vector4(i, j, k, 1f), i + size.y * j + size.y * size.z * k, new Vector3Int(i, j, k), true);
 
                 }
             }
@@ -51,9 +51,9 @@ public class VoxelHair : MonoBehaviour
     {
         GameObject tempObj;
         inActiveVoxels = new Stack<VoxelObj>(10000);
-        for(int i = 0; i < amountOfVoxels; i++)
+        for (int i = 0; i < amountOfVoxels; i++)
         {
-            tempObj = Instantiate(testObject);
+            tempObj = Instantiate(testObject, this.transform.parent);
             tempObj.SetActive(false);
             inActiveVoxels.Push(tempObj.GetComponent<VoxelObj>());
         }
@@ -63,24 +63,7 @@ public class VoxelHair : MonoBehaviour
     bool[,,] alreadyGoneThrough;
     void Optimize()
     {
-        //togoThrough = new Queue<Vector3Int>(300);
-        //togoThrough.Enqueue(new Vector3Int(0, 0, 0));
-        //position = new List<Vector3Int>(100);
-        //alreadyGoneThrough = new bool[size.x, size.y, size.z];
-        //alreadyGoneThrough[0, 0, 0] = true;
-
-        //int count = 0;
-        //do
-        //{
-        //    count++;
-        //    Vector3Int next = togoThrough.Dequeue();
-        //    if (hasEmptyNear(next))
-        //    {
-        //        position.Add(next);
-        //    }
-
-        //} while (togoThrough.Count > 0);
-        position = new List<Vector3Int>(size.x*size.y*size.z);
+        position = new List<Vector3Int>(size.x * size.y * size.z);
         //List<Vector3Int> nonPosition = new List<Vector3Int>();
         alreadyGoneThrough = new bool[size.x, size.y, size.z];
         alreadyGoneThrough[0, 0, 0] = true;
@@ -88,9 +71,9 @@ public class VoxelHair : MonoBehaviour
             for (int j = 0; j < size.y; j++)
                 for (int k = 0; k < size.x; k++)
                 {
-                    if(hasEmptyNear(new Vector3Int(i,j,k)) && voxels[i,j,k].hasHair)
+                    if (hasEmptyNear(new Vector3Int(i, j, k)) && voxels[i, j, k].hasHair)
                     {
-                        position.Add(new Vector3Int(i,j,k));
+                        position.Add(new Vector3Int(i, j, k));
                     }
                 }
 
@@ -99,21 +82,21 @@ public class VoxelHair : MonoBehaviour
         {
             ActivateVoxel(position[i]);
         }
-        for(int i = 0; i<activeVoxels.Count; i++)
+        for (int i = 0; i < activeVoxels.Count; i++)
         {
             int x = activeVoxels[i].position.x;
             int y = activeVoxels[i].position.y;
             int z = activeVoxels[i].position.z;
 
-            if (!voxels[x,y,z].isActive)
+            if (!voxels[x, y, z].isActive)
             {
                 DeactivateVoxel(activeVoxels[i]);
             }
         }
-        
+
     }
 
-    public void RemoveHair(Vector3 position,float distance)
+    public void RemoveHair(Vector3 position, float distance)
     {
         List<Voxel> VoxelsToGoThrough = new List<Voxel>(300);
         int counter = 0;
@@ -138,7 +121,6 @@ public class VoxelHair : MonoBehaviour
                         if (distance * distance > Vector3.SqrMagnitude(pos1 - voxelPos))
                         {
                             VoxelsToGoThrough.Add(voxels[i, j, k]);
-                            //VoxelsToGoThrough.Add(GetActiveVoxel(voxels[i, j, k].voxelIndex).GetComponent<VoxelObj>());
                         }
                     }
                 }
@@ -155,8 +137,8 @@ public class VoxelHair : MonoBehaviour
             //if(!neighBors.Contains(voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z]))
             //{
             //RemoveVoxel();
-            DeactivateVoxel(GetActiveVoxel(VoxelsToGoThrough[i].voxelIndex));
-            voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z].isActive = false;
+            RemoveVoxel(VoxelsToGoThrough[i].pos);
+            //voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z].isActive = false;
             voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z].hasHair = false;
             //DeactivateVoxel()
             //}
@@ -197,16 +179,16 @@ public class VoxelHair : MonoBehaviour
             DeactivateVoxel(obj);
             voxels[x, y, z].isActive = false;
         }
-        voxels[x, y, z].isActive = false;
-        voxels[x, y, z].hasHair = false;
     }
 
     void DeactivateVoxel(GameObject obj)
     {
-        if(obj != null)
+        if (obj != null)
         {
-            obj.SetActive(false);
-            inActiveVoxels.Push(obj.GetComponent<VoxelObj>());
+            VoxelObj temp = obj.GetComponent<VoxelObj>();
+            activeVoxels.Remove(temp);
+            temp.Deactivate();
+            inActiveVoxels.Push(temp);
         }
 
     }
@@ -218,7 +200,7 @@ public class VoxelHair : MonoBehaviour
     }
     GameObject GetActiveVoxel(int id)
     {
-        for (int i = 0; i<activeVoxels.Count; i++)
+        for (int i = 0; i < activeVoxels.Count; i++)
         {
             if (activeVoxels[i].index == id)
             {
@@ -236,7 +218,7 @@ public class VoxelHair : MonoBehaviour
         int y = pos.y;
         int z = pos.z;
         neighBors.Clear();
-        if(size.x > x + 1)
+        if (size.x > x + 1)
         {
             neighBors.Add(voxels[x + 1, y, z]);
         }
@@ -250,15 +232,15 @@ public class VoxelHair : MonoBehaviour
         }
         if (0 <= y - 1)
         {
-            neighBors.Add(voxels[x, y-1, z]);
+            neighBors.Add(voxels[x, y - 1, z]);
         }
         if (size.z > z + 1)
         {
-            neighBors.Add(voxels[x, y, z+1]);
+            neighBors.Add(voxels[x, y, z + 1]);
         }
         if (0 <= z - 1)
         {
-            neighBors.Add(voxels[x, y, z-1]);
+            neighBors.Add(voxels[x, y, z - 1]);
         }
         return neighBors;
     }
@@ -287,7 +269,6 @@ public class VoxelHair : MonoBehaviour
                         if (size * size > Vector3.SqrMagnitude(pos1 - voxelPos))
                         {
                             VoxelsToGoThrough.Add(voxels[i, j, k]);
-                            //VoxelsToGoThrough.Add(GetActiveVoxel(voxels[i, j, k].voxelIndex).GetComponent<VoxelObj>());
                         }
                     }
                 }
@@ -306,7 +287,7 @@ public class VoxelHair : MonoBehaviour
             //RemoveVoxel();
             //ActivateVoxel(VoxelsToGoThrough[i].pos);
             //voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z].isActive = true;
-            
+
             voxels[VoxelsToGoThrough[i].pos.x, VoxelsToGoThrough[i].pos.y, VoxelsToGoThrough[i].pos.z].hasHair = true;
             //DeactivateVoxel()
             //}
@@ -322,7 +303,7 @@ public class VoxelHair : MonoBehaviour
         bool hasEmptyNeighbor = false;
         if (size.x > x + 1)
         {
-            if (!voxels[x+1, y, z].hasHair)
+            if (!voxels[x + 1, y, z].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -331,7 +312,7 @@ public class VoxelHair : MonoBehaviour
         }
         if (0 <= x - 1)
         {
-            if (!voxels[x-1, y, z].hasHair)
+            if (!voxels[x - 1, y, z].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -340,7 +321,7 @@ public class VoxelHair : MonoBehaviour
         }
         if (size.y > y + 1)
         {
-            if (!voxels[x, y+1, z].hasHair)
+            if (!voxels[x, y + 1, z].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -349,7 +330,7 @@ public class VoxelHair : MonoBehaviour
         }
         if (0 <= y - 1)
         {
-            if (!voxels[x, y-1, z].hasHair)
+            if (!voxels[x, y - 1, z].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -358,7 +339,7 @@ public class VoxelHair : MonoBehaviour
         }
         if (size.z > z + 1)
         {
-            if (!voxels[x, y, z+1].hasHair)
+            if (!voxels[x, y, z + 1].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -367,7 +348,7 @@ public class VoxelHair : MonoBehaviour
         }
         if (0 <= z - 1)
         {
-            if (!voxels[x, y, z-1].hasHair)
+            if (!voxels[x, y, z - 1].hasHair)
                 hasEmptyNeighbor = true;
         }
         else
@@ -393,7 +374,7 @@ public struct Voxel
     public int voxelIndex;
     public bool isActive;
 
-    public Voxel(Vector4 info, int voxelIndex, Vector3Int pos,bool hasHair)
+    public Voxel(Vector4 info, int voxelIndex, Vector3Int pos, bool hasHair)
     {
         this.hasHair = hasHair;
         isActive = false;
