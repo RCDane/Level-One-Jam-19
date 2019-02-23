48a883fb1b0170a866848a5309ccc098fa662fd8
Jorgevy <jorgevy94@gmail.com>
2019-02-23 01:24:46 +0100

    Updated Scripts for Destructor and Constructor (Work in progress) Updated Script for Camera Movement (Work in progress)

---
+++ Assets/Scripts/DestructorMouse.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructorMouse : MonoBehaviour
+{
+
+    void OnMouseOver() // Triggers the following code when the mouse hovers over the object with this script attached to it
+    {
+        if (Input.GetMouseButton(0)) // Action started if the button pressed while hovering is the mouse left button
+        {
+            Destroy(this.gameObject); // Destroy the game object that has this script attached to it
+        }
+
+        if (Input.GetMouseButton(1))// Action started if the button pressed while hovering is the mouse right button
+        {
+            Invoke("MultiplyHair",0f);  // Calls the method "MultiplyHair" after 'delay' seconds, and then repeat every y seconds
+        }
+    }
+    void MultiplyHair() // Creates 6 clones of this game objects separated one unit each from one of the faces
+    {
+        Instantiate(this.gameObject, this.transform.position + new Vector3(1, 0, 0), Quaternion.identity);
+        Instantiate(this.gameObject, this.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        Instantiate(this.gameObject, this.transform.position + new Vector3(0, 0, 1), Quaternion.identity);
+        Instantiate(this.gameObject, this.transform.position + new Vector3(-1, 0, 0), Quaternion.identity);
+        Instantiate(this.gameObject, this.transform.position + new Vector3(0, -1, 0), Quaternion.identity);
+        Instantiate(this.gameObject, this.transform.position + new Vector3(0, 0, -1), Quaternion.identity);
+    }
+}
