6c5666a1cfe464051a6b8d1589d7cff573e7b78b
Nikolaj de Haan <contact@audiobits.pro>
2019-02-23 14:17:20 +0100

    FMOD stuff added
    - Added scene for audio testing
    - Added ambience loop
    - Added AudioManager script
    - Added FMOD listener
    - Added a scissor test game object, with script trigging 3D sound from the AudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //[Header("FMOD One Shot Events")]
+    [Header("FMOD One Shot Events")]
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string ambienceLoop;
+
+    [Header("FMOD Events with parameters")]
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string musicEvent;
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string scissorCutEvent;
+
+    [Header("FMOD Parameters")]
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string haircutProgress;
+
+    //------ Her laves mine FMOD instances ------
+    private FMOD.Studio.EventInstance scissorCutInstance;
+
+
+    void Start()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(ambienceLoop);
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown("space"))
+        //{
+        //    ScissorCut();
+        //    print("space key was pressed");
+        //}
+
+    }
+
+    //------ Methods for FMOD instances ------
+
+
+    //Husk at sende gameObject med som argument
+    public void ScissorCut(GameObject objectToAttachTo)
+    {
+        scissorCutInstance = FMODUnity.RuntimeManager.CreateInstance(scissorCutEvent);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(scissorCutInstance, objectToAttachTo.transform, objectToAttachTo.GetComponent<Rigidbody>());
+        scissorCutInstance.start();
+        scissorCutInstance.release();
+    }
+}
