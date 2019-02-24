5779bde89b29ff90d091bd5329e8db37fed05799
Jorgevy <jorgevy94@gmail.com>
2019-02-24 11:36:19 +0100

    Setted up 3D models following the mouse

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Rigidbody rb;
+    private Vector3 direction;
+    private float moveSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector3(direction.x * moveSpeed, direction.y * moveSpeed, direction.z * moveSpeed);
+    }
+}
