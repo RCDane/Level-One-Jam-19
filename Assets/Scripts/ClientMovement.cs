e09db5c80fcb11e28c95e2d11d2f1892330e70ab
Nikolaj de Haan <contact@audiobits.pro>
2019-02-24 11:54:59 +0100

    Sound added to the clientmovement script

--- Assets/Scripts/ClientMovement.cs
+++ Assets/Scripts/ClientMovement.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ClientMovement : MonoBehaviour
 {
+    public AudioManager audioManagerObject;
     // Start is called before the first frame update
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.Rotate(0, 90, 0);
+            audioManagerObject.ChairRotate();
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             transform.Rotate(0, -90, 0);
+            audioManagerObject.ChairRotate();
         }
     }
 }
